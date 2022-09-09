b9676a7b3031acb795560ed20e9b989548c515f7
ZachAdventure2 <113042161+ZachAdventure2@users.noreply.github.com>
2022-09-09 10:38:46 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,3 +7,8 @@ input.onButtonPressed(Button.B, function () {
     pins.digitalWritePin(DigitalPin.P1, 0)
 })
 basic.showIcon(IconNames.Skull)
+basic.forever(function () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+})
