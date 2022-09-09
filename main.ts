3e5daa3ba15eae00afc0dc3218dab121187e83c5
ZachAdventure2 <113042161+ZachAdventure2@users.noreply.github.com>
2022-09-09 10:39:53 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -11,4 +11,6 @@ basic.forever(function () {
     pins.digitalWritePin(DigitalPin.P1, 1)
     basic.pause(1000)
     pins.digitalWritePin(DigitalPin.P1, 0)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P1, 1)
 })
